8a24afd7c2a4a25628d781b69ee014142aa9da4e
doman93 <manhhungdo.dmh@gmail.com>
2020-12-15 01:31:02 +0100

    Hello

--- Test_code/Test_code/Test_code.cpp
+++ Test_code/Test_code/Test_code.cpp
@@ -1,80 +1,31 @@
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-#include <map>
-#include <string>
+#include <iostream>
 #include <algorithm>
-#include <set>
-#include <cassert>
 using namespace std;
 
-struct Node {
-	Node* next;
-	Node* prev;
-	int value;
-	int key;
-	Node(Node* p, Node* n, int k, int val) :prev(p), next(n), key(k), value(val) {};
-	Node(int k, int val) :prev(NULL), next(NULL), key(k), value(val) {};
-};
 
-class Cache {
-
-protected:
-	map<int, Node*> mp; //map the key to the node in the linked list
-	int cp;  //capacity
-	Node* tail; // double linked list tail pointer
-	Node* head; // double linked list head pointer
-	virtual void set(int, int) = 0; //set function
-	virtual int get(int) = 0; //get function
-
-};
-
-#include <deque>
-class LRUCache : public Cache {
-public:
-	deque<pair<int, int> > v;
-	LRUCache(const int &x) {
-		cp = x;
-	}
-	int get(int key) {
-		for (int i = 0; i < v.size(); i++) {
-			if (v[i].first == key) {
-				return v[i].second;
-			}
-		}
-		return -1;
-	}
-	void set(int key, int value) {
-		int i = 0;
-		for (; i < v.size(); i++) {
-			if (v[i].first == key) {
-				v.erase(v.begin() + i);
-				break;
-			}
-		}
-
-		if (i == cp)
-			v.pop_back();
-
-		v.push_front(make_pair(key, value));
-	}
-};
 int main() {
-	int n, capacity, i;
-	cin >> n >> capacity;
-	LRUCache l(capacity);
-	for (i = 0; i < n; i++) {
-		string command;
-		cin >> command;
-		if (command == "get") {
-			int key;
-			cin >> key;
-			cout << l.get(key) << endl;
-		}
-		else if (command == "set") {
-			int key, value;
-			cin >> key >> value;
-			l.set(key, value);
-		}
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int N;
+	cin >> N;
+	vector<int> X;
+	int num;
+	for (int i = 0; i < N; ++i) {
+		cin >> num;
+		X.push_back(num);
+	}
+	int Q;
+	cin >> Q;
+	for (int i = 0; i < Q; ++i) {
+		int tmp;
+		cin >> tmp;
+		vector<int>::iterator low = lower_bound(X.begin(), X.end(), tmp);
+		if (X[low - X.begin()] == tmp)
+			cout << "Yes " << (low - X.begin() + 1) << endl;
+		else
+			cout << "No " << (low - X.begin() + 1) << endl;
 	}
 	return 0;
 }
